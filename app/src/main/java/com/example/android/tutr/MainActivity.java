76d77ce87f0187342e031dd3e693532c32a93978
JKazma <joey.kazma@outlook.com>
2016-02-16 18:12:16 -0500

    Search Feature - Added unfinished UI for the search and search results

--- app/src/main/java/com/example/android/tutr/MainActivity.java
+++ app/src/main/java/com/example/android/tutr/MainActivity.java
@@ -2,8 +2,7 @@ package com.example.android.tutr;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.view.View.OnClickListener;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,9 +10,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -83,6 +87,7 @@ public class MainActivity extends AppCompatActivity
         catch (Exception e){}
 
     }
+
     /**
      * Execute item code when it is pressed on navigation drawer
      * @param item
@@ -118,5 +123,63 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    private void addListenerToSearchButton(){
+        ImageButton searchButton = (ImageButton) findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+                // FETCH STUFF FROM THE INTERNET
+                // UPDATE XX RESULTS FOUND
+                populateResultsList();
+            }
+        });
+    }
+
+    private void addListenerToOrderingButtons(){
+        Button hourlyButton = (Button) findViewById(R.id.hourly_button);
+        Button ratingButton = (Button) findViewById(R.id.rating_button);
+
+        hourlyButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
+        ratingButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
+    }
+
+
+    private void populateResultsList(){
+        LinearLayout searchResultLayout = (LinearLayout) findViewById(R.id.searchResultLayout);
+        //searchResultLayout.setVisibility(View.GONE);
+
+        //Populate list view test
+        String[] test = {"test1", "test2", "test3", "test1", "test2", "test3", "test1", "test2", "test3", "test1", "test2", "test3",
+                "test1", "test2", "test3", "test1", "test2", "test3", "test1", "test2", "test3", "test1", "test2", "test3",
+                "test1", "test2", "test3", "test1", "test2", "test3", "test1", "test2", "test3", "test1", "test2", "test3"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.search_result_item, test);
+        ListView list = (ListView) findViewById(R.id.search_result_list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id){
+                TextView textView = (TextView) viewClicked;
+                String message = position + " " + textView.getText().toString();
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+
+        });
     }
 }
