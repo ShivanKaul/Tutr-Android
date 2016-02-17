9e65750db0be3424838608a42b4aa4fbe5220b20
Dylan Andrianantenainaa <Dylan Andrianantenainaa>
2016-02-17 17:29:49 -0500

    View Profile UI - added unfinished UI for view tutor profile

---
+++ app/src/main/java/com/example/android/tutr/viewTutor.java
@@ -0,0 +1,13 @@
+package com.example.android.tutr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class viewTutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_tutor);
+    }
+}
