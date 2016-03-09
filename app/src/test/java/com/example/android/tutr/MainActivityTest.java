edc83f13cceaa6c2e74e91a9594571c9fe5a2043
Tong-Yi <tong.yi@mail.mcgill.ca>
2016-03-09 12:21:04 -0500

    Search - inputChecker Junit

---
+++ app/src/test/java/com/example/android/tutr/MainActivityTest.java
@@ -0,0 +1,29 @@
+package com.example.android.tutr;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by yito on 2/28/16.
+ */
+public class MainActivityTest extends TestCase {
+    
+    MainActivity main = new MainActivity();
+
+    public void testInputChecker(){
+        String nameAlphabet = "Jhonny";
+        String nameNumber = "John123";
+        String nameEmpty = "";
+        String courseNotEmpty = "ECSE421";
+        String courseEmpty = "";
+
+        assertEquals(main.inputChecker(nameEmpty, courseEmpty), 0);
+        assertEquals(main.inputChecker(nameAlphabet, courseEmpty), 1);
+        assertEquals(main.inputChecker(nameEmpty, courseNotEmpty), 2);
+        assertEquals(main.inputChecker(nameAlphabet, courseNotEmpty), 3);
+        assertEquals(main.inputChecker(nameNumber, courseEmpty), 4);
+        assertEquals(main.inputChecker(nameNumber, courseNotEmpty), 4);
+
+
+
+    }
+}
