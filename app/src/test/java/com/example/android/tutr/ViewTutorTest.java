3522d2ca4caed840ee54a14c2c69f9cedb7b045e
Dylan Andrianantenainaa <Dylan Andrianantenainaa>
2016-03-09 19:13:50 -0500

    forgot to remove a print statement on viewTutorTest

--- app/src/test/java/com/example/android/tutr/ViewTutorTest.java
+++ app/src/test/java/com/example/android/tutr/ViewTutorTest.java
@@ -13,7 +13,7 @@ public class ViewTutorTest extends TestCase {
     //   averageRating(int new_counter, float rating, double old_rating, double rating_counter);
     @Test
     public void testAverage() {
-        System.out.println();
+        assertEquals(2.57, ViewTutor.averageRating(10, 5.0f, 2.3, 9.0), 0.001);
         assertEquals(3.875, ViewTutor.averageRating(4, 3.5f, 4.0, 3.0), 0.001);
         assertEquals(4.8909, ViewTutor.averageRating(11, 3.8f, 5.0, 10.0), 0.001);
         assertEquals(4.117, ViewTutor.averageRating(51, 0.00f, 4.2,50.0), 0.001);
